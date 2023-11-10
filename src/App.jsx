7b79b539bfc5a303e3cf427f3bc687778b04c87e
SarthakMittal1 <sarthak.mittal180@nmims.edu.in>
2023-11-10 13:20:02 +0530

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Sarthak Mittal",
+  title: "Tech-Entrepreneur",
+  email: "mittalsarthak863@gmail.com",
+  gitHub: "SarthakMittal1",
+  instagram: "sarthak_mittal_1",
+  linkedIn: "sarthak-microsoft",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
